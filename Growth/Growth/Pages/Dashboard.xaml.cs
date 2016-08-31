29f8747490feab599eed2360f24ccd6099ea5e2c
Ahmad Mustofa <buffonmustofa@gmail.com>
2016-08-31 17:03:32 +0700

    mari ngendas sampe ga iling opo progress e

--- Growth/Growth/Pages/Dashboard.xaml.cs
+++ Growth/Growth/Pages/Dashboard.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Growth.Master;
+using Growth.Helper;
+using System.Collections.ObjectModel;
 
 namespace Growth.Pages
 {
@@ -20,9 +23,27 @@ namespace Growth.Pages
     /// </summary>
     public partial class Dashboard : Page
     {
+        private class Sales
+        {
+            public string Name { set; get; }
+            public string Area { set; get; }
+            public int TotalVisit { set; get; }
+            public int EffectiveCall { set; get; }
+            public Sales(string nama, string area, int total, int effect)
+            {
+                Name = nama;
+                Area = area;
+                TotalVisit = total;
+                EffectiveCall = effect;
+            }
+        }
         public Dashboard()
         {
             InitializeComponent();
+            List<Sales> sales = new List<Sales>();
+            sales.Add(new Sales("ahmad", "sumenep", 10, 5));
+            sales.Add(new Sales("mustofa", "sumenep", 7, 3));
+            listSales.ItemsSource = sales;
         }
     }
 }
--- WSDesktop.php
+++ WSDesktop.php
@@ -56,4 +56,56 @@ class WSDesktop extends Controller
         $dataUser = array("area"=>$area,"competitor"=>$competitor,"distributor"=>$distributor,"konfigurasi"=>$konfigurasi,"kota"=>$kota,"logging"=>$logging,"outlet"=>$outlet,"photo"=>$photo,"produk"=>$produk,"takeorder"=>$takeorder,"tipe"=>$tipe,"tipe_photo"=>$tipePhoto,"user"=>$user,"visitplan"=>$visitplan);
         return Response::json($dataUser,200);
     }
+    public function insertOutlet()
+    {
+    	$id = -1;
+        $data=new Outlet;
+        $kota = Kota::where("id",Input::get('kd_kota'))->first();
+        if(!Input::has('nm_outlet'))
+        {
+            $out['status'] = "no data sent";
+            return Response::json($out,500);
+        }
+        $konfig = Konfigurasi::first();
+        $data->kd_dist=Input::get('kd_dist');
+        $data->kd_kota=Input::get('kd_kota');
+        $data->kd_area=$kota->kd_area;
+        $data->kd_user=Input::get('kd_user');
+        $data->nm_outlet=Input::get('nm_outlet');
+        $data->almt_outlet=Input::get('almt_outlet');
+        $data->kd_tipe=Input::get('kd_tipe');
+        $data->rank_outlet=Input::get('rank_outlet');
+        $data->kodepos=Input::get('telp_outlet');
+        $data->reg_status=Input::get('reg_status');
+        $data->latitude=Input::get('latitude');
+        $data->longitude=Input::get('longitude');
+        $data->nm_pic=Input::get('nama_pic');
+        $data->tlp_pic=Input::get('tlp_pic');
+        $data->toleransi_long=$konfig->toleransi_max;
+        $success = $data->save();
+        $path = 'image_upload/outlet/';
+        $base =Input::get('foto');
+        $binary = base64_decode($base);
+        header('Content-Type: bitmap; charset=utf-8');
+
+        $f = finfo_open();
+        $mime_type = finfo_buffer($f, $binary, FILEINFO_MIME_TYPE);
+        $mime_type = str_ireplace('image/', '', $mime_type);
+
+        $filename =  strval($data->kd_outlet).date('_YmdHis'). '.' . $mime_type;
+        $file = fopen($path . $filename, 'wb');
+        if (fwrite($file, $binary)) {
+            $data->update(array('foto_outlet' => $filename));
+            fclose($file);
+        }
+        $logging = new Logging;
+        $logging->kd_user = Input::get("kd_user");
+        $logging->description = "Register outlet ".Input::get("nm_outlet");
+        $logging->log_time = Input::get('tgl_upload');
+        $logging->detail_akses = "desktop";
+        $logging->save();
+        $out['status'] = "success";
+        $out['id'] = $data->kd_outlet;
+        return Response::json($out,201); 
+    }
 }
